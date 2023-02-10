43e7bf2f49c22280e59474e47ad119299e909f5e
itaegjin <tag101@hanmail.net>
2023-02-10 21:43:11 +0900

    011_Hello Servlet

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	// 서블릿 자동 등록 (하위 페키지를 찾아서 서블릿 등록 실행)
 @SpringBootApplication
 public class ServletApplication {
 
